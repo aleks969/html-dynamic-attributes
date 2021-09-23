3c06f452fdd473e6ef39b37b7232e28fd02735f5
aleks <aleks.mihaylov69@gmail.com>
2021-09-23 13:42:31 +0300

    fix the selectors

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const product = document.querySelector('.product');
+  const product = document.querySelector('.card.product');
   const price = document.querySelector('.price');
   product.setAttribute('data-price', price.textContent)
 
